2ed61c68b42051d7ae3cb77b2ed05c39f119c48a
adeshkin <adeshkin.vi@phystech.edu>
2022-03-27 12:29:05 +0300

    augs

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -32,6 +32,10 @@ def get_transforms(img_size):
     transforms = dict()
     train_transform = A.Compose([
         A.Resize(height=img_size[0], width=img_size[1]),
+        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+        A.HorizontalFlip(p=0.5),
+        A.CoarseDropout(max_holes=2, min_height=10, max_height=20, min_width=10, max_width=20, p=0.5),
+        A.GaussNoise(var_limit=(10, 50), p=0.5),
         A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
         A.RandomBrightnessContrast(p=0.5),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
