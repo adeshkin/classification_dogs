135f1b7c59fc59af140a70c2847af9c5a6ee7f4d
adeshkin <adeshkin.vi@phystech.edu>
2022-03-28 13:29:41 +0300

    semi-final

--- scripts/model.py
+++ scripts/model.py
@@ -5,10 +5,7 @@ import torch.nn as nn
 def get_model(model_name, num_classes):
     if model_name in torchvision.models.__dict__:
         model = torchvision.models.__dict__[model_name](pretrained=True)
-        if 'resnet' in model_name:
-            model.fc = nn.Linear(model.fc.in_features, num_classes)
-        elif 'efficientnet' in model_name:
-            model.fc = nn.Linear(1280, num_classes)
+        model.fc = nn.Linear(model.fc.in_features, num_classes)
     else:
         raise NotImplementedError('Model is not implemented')
 
